d1ce17591e6cdd5ddc777f558cf208b60d8e0297
vle <obentoka@gmail.com>
2020-03-06 20:14:06 -0500

    did something

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,35 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T>{
+
+    T[] array;
+
+    public ArrayUtility(T[] array){
+        this.array = array;
+    }
+
+    public Integer getNumberOfOccurrences(T item){
+        Integer count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i].equals(item))
+                count++;
+        }
+        return count;
+    }
+
+    public T[] removeValue(T item){
+        Integer occurance = getNumberOfOccurrences(item);
+        T[] retArray = (T[]) Array.newInstance(item.getClass(), array.length-occurance);
+        Integer retArrayCounter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(!array[i].equals(item))
+                retArray[retArrayCounter] = array[i];
+        }
+        return retArray;
+    }
 }
